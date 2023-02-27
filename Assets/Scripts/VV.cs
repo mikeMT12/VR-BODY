f08d1f823e399c794eae466b27015413a0d93f75
mikeMT12 <95416180+mikeMT12@users.noreply.github.com>
2023-02-27 17:17:33 +0300

    2
    Version with problem in triggers

---
+++ Assets/Scripts/VV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VV : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+
+    
+        if (other.gameObject.GetComponent<TZone>())
+        {
+            Debug.Log(4);
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
