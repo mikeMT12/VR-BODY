98971d9100cbf4dd746653808e0e4d1fe5d298d3
mikeMT12 <95416180+mikeMT12@users.noreply.github.com>
2023-03-12 23:16:10 +0300

    badddd
    q

--- Assets/Scripts/Hand1.cs
+++ Assets/Scripts/Hand1.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 public class Hand1 : MonoBehaviour
 {
     public GameObject[] points;
+    [SerializeField] private Hand1 _leftHand;
+    [SerializeField] private Hand1 _rightHand;
     [SerializeField] private GameManager1 gameManager;
+
+
     public int stage = 0;
-    
+
     private void OnTriggerEnter(Collider other)
     {
+        
         gameManager.nowTime = 0;
         //if (points.Contains(other.name))
         //if (other.gameObject.GetComponent<PointForTriggers>())
-        foreach (GameObject zone in points)
+        foreach (GameObject zone in _leftHand.points)
         {
-            if(zone.name == other.name)
+            Debug.Log(zone.tag);
+            int index = Array.IndexOf(_leftHand.points, zone);
+            //if(other.gameObject.tag == zone.gameObject.tag)
+            if (index == _leftHand.stage)
+            {
+                points[stage].SetActive(false);
+                stage += 1;
+                gameManager.SetStage();
+            }
+        }
+        foreach (GameObject zone1 in _rightHand.points)
+        {
+            Debug.Log(zone1.name);
+            int index1 = Array.IndexOf(_rightHand.points, zone1);
+            Debug.Log(index1);
+            //if (other.gameObject.tag == zone1.gameObject.tag)
+            if(index1 == _rightHand.stage)
             {
                 points[stage].SetActive(false);
                 stage += 1;
